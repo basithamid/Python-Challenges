8ba72a0d356fe5ea870f0ed77ba7beaf4aacf7f2
Basit Hamid <basithamid@gmail.com>
2018-01-10 18:54:02 +0530

    Update code.py

--- code.py
+++ code.py
@@ -7,11 +7,11 @@ for char in str1:
     if char is not 'w' and char is not 'v':
         min_count = min_count + 1
         max_count = max_count + 1
-        new_str = new_str + char
+
     elif char is 'w':
         min_count = min_count + 1
         max_count = max_count + 2
-        new_str = new_str + "vv"
+        
     elif char is 'v':
         max_count = max_count + 1
         v_count = v_count + 1
