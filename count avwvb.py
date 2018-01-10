6aa0d97fa9a25b1527910c3e6a23bd47ede75c35
Basit Hamid <basithamid@gmail.com>
2018-01-10 23:05:13 +0530

    Update count avwvb.py

--- count avwvb.py
+++ count avwvb.py
@@ -11,13 +11,15 @@ for char in str1:
     elif char is 'w':
         min_count = min_count + 1
         max_count = max_count + 2
-        
+
     elif char is 'v':
         max_count = max_count + 1
-        v_count = v_count + 1
-        if v_count == 2:
-            v_count = 0
+        if v_count == 0:
+            v_count = v_count + 1
             min_count = min_count + 1
+
+        elif v_count == 1:
+            v_count = 0
 
 
 print("min count {}".format(min_count))
