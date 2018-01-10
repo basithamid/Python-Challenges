f3e04c65239dbf7cac89226241849d0d5b024027
Basit Hamid <basithamid@gmail.com>
2018-01-10 18:44:02 +0530

    Update code.py

--- code.py
+++ code.py
@@ -1,27 +1,24 @@
 str1 = input()
 min_count = 0
 max_count = 0
-new_str = ""
+v_count = 0
 
 for char in str1:
-    if char is not 'w':
+    if char is not 'w' and char is not 'v':
+        min_count = min_count + 1
         max_count = max_count + 1
         new_str = new_str + char
-    else:
+    elif char is 'w':
+        min_count = min_count + 1
         max_count = max_count + 2
         new_str = new_str + "vv"
-
-v = 0
-for char in new_str:
-    if char is not 'v':
-        min_count = min_count + 1
     elif char is 'v':
-        if v == 0:
-            v = 1
-
-        elif v == 1:
+        max_count = max_count + 1
+        v_count = v_count + 1
+        if v_count == 2:
+            v_count = 0
             min_count = min_count + 1
-            v = 0
+
 
 print("min count {}".format(min_count))
 print("max count {}".format(max_count))
