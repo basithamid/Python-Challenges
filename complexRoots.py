aa6b29ddc867cfaec787803f60c3aa302a2f3951
Basit Hamid <basithamid@gmail.com>
2018-01-18 12:34:08 +0530

    Update complexRoots.py

--- complexRoots.py
+++ complexRoots.py
@@ -12,8 +12,8 @@ n = int(input())
 for i in range(0, n):
     abc = list(map(int, input().split()))
     root = cmath.sqrt((abc[1]*abc[1]) - (4 * abc[0] * abc[2]))
-    strroot = str(root)
-    ind = strroot.index('j') - 1
-    if strroot[ind] == '0' and strroot[ind-1] == '+':
+    img = root.imag
+    print(img)
+    if img != 0:
         count = count + 1
 print(count)
